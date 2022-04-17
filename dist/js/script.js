d1cf3393789025a65f902f69ab1e22b18e78a43b
Mbareb Leonardo <75684833+mbareb-leonardo14@users.noreply.github.com>
2022-04-18 00:22:51 +0700

    update dark mode + feature improvements

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,35 +1,35 @@
 //Navbar fixed
-window.onscroll = function() {
-  const header = document.querySelector('header');
-  const fixedNav = header.offsetTop;
-  const toTop = document.querySelector('#to-top')
+window.onscroll = function () {
+	const header = document.querySelector('header');
+	const fixedNav = header.offsetTop;
+	const toTop = document.querySelector('#to-top');
 
-  if(window.pageYOffset > fixedNav) {
-    header.classList.add('navbar-fixed');
-    toTop.classList.remove('hidden');
-    toTop.classList.add('flex');
-  } else {
-    header.classList.remove('navbar-fixed');
-    toTop.classList.remove('flex');
-    toTop.classList.add('hidden');
-  }
-}
+	if (window.pageYOffset > fixedNav) {
+		header.classList.add('navbar-fixed');
+		toTop.classList.remove('hidden');
+		toTop.classList.add('flex');
+	} else {
+		header.classList.remove('navbar-fixed');
+		toTop.classList.remove('flex');
+		toTop.classList.add('hidden');
+	}
+};
 // Hamburger
 
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function() {
-  hamburger.classList.toggle('hamburger-active');
-  navMenu.classList.toggle('hidden');
+hamburger.addEventListener('click', function () {
+	hamburger.classList.toggle('hamburger-active');
+	navMenu.classList.toggle('hidden');
 });
 
 // klik diluar hamburger
 window.addEventListener('click', function (e) {
-  if(e.target != navMenu && e.target != hamburger) {
-    hamburger.classList.remove('hamburger-active');
-    navMenu.classList.add('hidden')
-  }
+	if (e.target != navMenu && e.target != hamburger) {
+		hamburger.classList.remove('hamburger-active');
+		navMenu.classList.add('hidden');
+	}
 });
 
 // darkmode toggle
@@ -37,19 +37,22 @@ const darkToggle = document.querySelector('#dark-toggle');
 const html = document.querySelector('html');
 
 darkToggle.addEventListener('click', function () {
-  if (darkToggle.checked) {
-    html.classList.add('dark');
-    localStorage.theme = 'dark';
-  } else {
-    html.classList.remove('dark');
-    localStorage.theme = 'light';
-  }
+	if (darkToggle.checked) {
+		html.classList.add('dark');
+		localStorage.theme = 'dark';
+	} else {
+		html.classList.remove('dark');
+		localStorage.theme = 'light';
+	}
 });
 
 //  move the toggle position according to the mode
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  darkToggle.checked = true;
+if (
+	localStorage.theme === 'dark' ||
+	(!('theme' in localStorage) &&
+		window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+	darkToggle.checked = true;
 } else {
-  darkToggle.checked = false;
+	darkToggle.checked = false;
 }
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
 module.exports = {
-  content: [
-    'index.html'
-  ],
-  darkMode: 'class',
-  theme: {
-    container: {
-      center: true,
-      padding: '16px',
-    },
-    extend: {
-      colors: {
-        primary: '#7C3AED',
-        secondary: '#71717A',
-        dark: '#18181B',
-      },
-      screens: {
-        '2xl': '1320px',
-      },
-    },
-  },
-  plugins: [],
+	content: ['index.html'],
+	darkMode: 'class',
+	theme: {
+		container: {
+			center: true,
+			padding: '16px',
+		},
+		extend: {
+			colors: {
+				primary: '#7C3AED',
+				secondary: '#71717A',
+				dark: '#18181B',
+			},
+			screens: {
+				'2xl': '1320px',
+			},
+		},
+	},
+	plugins: [],
 };

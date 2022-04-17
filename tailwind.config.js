a506e4bd5d79dc151b4157d9ce0c5bf51bdb9c12
Mbareb Leonardo <75684833+mbareb-leonardo14@users.noreply.github.com>
2022-04-17 15:07:03 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    'index.html'
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#7C3AED',
+        secondary: '#71717A',
+        dark: '#18181B',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
